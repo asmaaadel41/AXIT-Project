836658c7adb91654b10cea2d84a0113708f52bd4
mahmoudadel44 <mrmahmoudadel199@gmail.com>
2021-07-03 18:33:27 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+
+    "use strict";
+
+    $(window).scroll(function (){
+        var navbar = $('.navbar');
+        ($(window).scrollTop() >= navbar.height()) ?  navbar.addClass('scrolled') :  navbar.removeClass('scrolled');
+    });
+
+    //Deal With Tabs
+    $('.tab-switch li').click(function(){
+        //Add Selected Class To Active Link 
+        $(this).addClass('selected').siblings().removeClass('selected');
+        //Hide All Divs
+        $('.tabs-section .tabs-content > div').hide();
+        //Show Div Content With This Link
+        $($(this).data('class')).show();
+    });
+
+});
